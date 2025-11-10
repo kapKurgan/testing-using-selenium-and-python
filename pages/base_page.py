362638d2d907dfe7610a77c12ed13277d64e4e8f
kkggaa45 <kkggaa45@yandex.ru>
2025-11-10 18:00:45 +0500

    Проверка элемента на странице

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,25 @@
+from selenium.common.exceptions import NoSuchElementException  # используется методом is_element_present
 
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         # Конструктор класса.
-        # :param browser:
-        # :param url:
+        # browser -
+        # url -
+        # timeout - команда для неявного ожидания со значением по умолчанию в 10
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         # открыть нужную страницу, используя метод get()
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        # метод is_element_present, в котором будем перехватывать исключение. В него будем передавать два аргумента:
+        # как искать (css, id, xpath и тд) и собственно что искать (строку-селектор)
+        # NoSuchElementException - исключение
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -5,3 +5,8 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        # метод, который будет проверять наличие ссылки.
+        # будем называть их should_be_(название элемента)
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Ссылка '#login_link' для входа в систему не представлена"
