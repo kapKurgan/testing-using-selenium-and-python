11186fc0566cfb7adec0d1c9585814a26e67b97b
kkggaa45 <kkggaa45@yandex.ru>
2025-11-11 16:30:07 +0500

    Элементы страниц в паттерне Page Object

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,3 +1,5 @@
+# из файла locators.py импортируйте класс с локаторами
+from .locators import MainPageLocators
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
@@ -9,4 +11,7 @@ class MainPage(BasePage):
     def should_be_login_link(self):
         # метод, который будет проверять наличие ссылки.
         # будем называть их should_be_(название элемента)
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Ссылка '#login_link' для входа в систему не представлена"
+        # у *MainPageLocators.LOGIN_LINK символ *, он указывает на то, что мы передали именно пару, и этот кортеж
+        # нужно распаковать
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Ссылка '#login_link' для входа в систему не представлена"
+
