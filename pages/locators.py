1c721c58dc3268087a914995c6d377d1bd3aedff
kkggaa45 <kkggaa45@yandex.ru>
2025-11-11 20:13:58 +0500

    Реализация LoginPage

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,23 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+# селекторы к формам регистрации и логина
+class LoginPageLocators():
+    # Войти - Адрес электронной почты
+    LOGIN_INPUT_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    # Войти - Пароль
+    LOGIN_INPUT_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    # Войти - Я забыл пароль
+    LOGIN_FORGOT_PASSWORD = (By.CSS_SELECTOR, "#login_form > p > a")
+    # Войти - Войти
+    LOGIN_BUTTON_LOGIN = (By.CSS_SELECTOR, "button[name=login_submit]")
+
+    # Зарегистрироваться - Адрес электронной почты
+    REGISTER_INPUT_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    # Зарегистрироваться - Пароль
+    REGISTER_INPUT_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    # Зарегистрироваться - Повторите пароль
+    REGISTER_INPUT_PASSWORD_CONFIRM = (By.CSS_SELECTOR, "#id_registration-password2")
+    # Зарегистрироваться - Зарегистрироваться
+    REGISTER_BUTTON_REGISTER = (By.CSS_SELECTOR, "button[name=registration_submit]")
---
+++ pages/login_page.py
@@ -0,0 +1,29 @@
+# из файла locators.py импортируйте класс с локаторами
+from .locators import LoginPageLocators
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # проверка на корректный url адрес
+        url = self.browser.current_url
+        assert "login" in url, "url адрес "+url+" не содержит 'login'"
+
+    def should_be_login_form(self):
+        # проверка, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_INPUT_EMAIL), ("Селектор 'LOGIN_INPUT_EMAIL' для входа в систему не представлен")
+        assert self.is_element_present(*LoginPageLocators.LOGIN_INPUT_PASSWORD), ("Селектор 'LOGIN_INPUT_PASSWORD' для входа в систему не представлен")
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORGOT_PASSWORD), ("Селектор 'LOGIN_FORGOT_PASSWORD' для входа в систему не представлен")
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON_LOGIN), ("Селектор 'LOGIN_BUTTON_LOGIN' для входа в систему не представлен")
+
+    def should_be_register_form(self):
+        # проверка, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_EMAIL), ("Селектор 'REGISTER_INPUT_EMAIL' для регистрации в систему не представлен")
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_PASSWORD), ("Селектор 'REGISTER_INPUT_PASSWORD' для регистрации в систему не представлен")
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_PASSWORD_CONFIRM), ("Селектор 'REGISTER_INPUT_PASSWORD_CONFIRM' для регистрации в систему не представлен")
+        assert self.is_element_present(*LoginPageLocators.REGISTER_BUTTON_REGISTER), ("Селектор 'REGISTER_BUTTON_REGISTER' для регистрации в систему не представлен")
