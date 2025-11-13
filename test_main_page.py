a0500c795c0816b313501ff19fd1b2b7aea6e1c6
kkggaa45 <kkggaa45@yandex.ru>
2025-11-13 20:02:01 +0500

    Группировка тестов: setup

--- test_main_page.py
+++ test_main_page.py
@@ -1,25 +1,31 @@
 # pytest -v --tb=line --language=en test_main_page.py
+# pytest -v --tb=line --language=en -m login_guest test_main_page.py
 
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import time
+import pytest
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)                          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                                             # открываем страницу
-    page.go_to_login_page()                                 # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)    # передаем объект драйвера для работы с браузером, а в качестве url передаем текущий адрес
-    login_page.should_be_login_page()                       # проверка на корректный url адрес should_be_login_url из login_page.py
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)                          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                                             # открываем страницу
+        page.go_to_login_page()                                 # выполняем метод страницы — переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)    # передаем объект драйвера для работы с
+        # браузером, а в качестве url передаем текущий адрес
+        login_page.should_be_login_page()                       # проверка на корректный url адрес
+        # should_be_login_url из login_page.py
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 def test_guest_should_see_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,45 @@
 # pytest -v -s --tb=line --language=en test_product_page.py
+# pytest -v -s --tb=line --language=en -m login test_product_page.py
 
 from selenium.webdriver.common.by import By
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 import time
 import pytest
+
+@pytest.mark.login
+class TestLoginFromProductPage():
+    # Чтобы функция запускалась автоматически перед каждым тест-кейсом, нужно пометить её как @pytest.fixture
+    # с параметрами scope="function", что значит запускать на каждую функцию, и autouse=True, что значит
+    # запускать автоматически без явного вызова фикстуры
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        print("\n***** 0")
+        # self.product = ProductFactory(title = "Best book created by robot")
+        # # создаем по апи
+        # self.link = self.product.link
+        yield
+        print("\n***** 1")
+        # # после этого ключевого слова начинается teardown
+        # # выполнится после каждого теста в классе
+        # # удаляем те данные, которые мы создали
+        # self.product.delete()
+
+    # тесты вида "гость может перейти на страницу логина со страницы Х"
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        print("\n***** test_guest_can_go_to_login_page_from_product_page")
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+
+    # тесты вида "гость может перейти на страницу логина со страницы Х"
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        print("\n***** test_guest_should_see_login_link_on_product_page")
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
 def test_guest_can_add_product_to_basket(browser, link):
@@ -41,20 +76,6 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.product_add_to_basket()            # в карточке товара - цена, наименование товара
     page.press_add_to_basket_button()       # нажать кнопку "Добавить в корзину"
     page.should_be_message_of_is_disappeared()
-
-# тесты вида "гость может перейти на страницу логина со страницы Х"
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-# тесты вида "гость может перейти на страницу логина со страницы Х"
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
 
 # Гость открывает страницу товара
 # Переходит в корзину по кнопке в шапке
