499dcf2e65888d2e974e5810aae0599f6fcc0307
kapKurgan <kkaapp@yandex.ru>
2025-12-28 19:21:09 +0500

    Настройка тестов на CI: GitHub Actions

--- conftest.py
+++ conftest.py
@@ -1,25 +1,48 @@
 import pytest
 from selenium import webdriver
-#from selenium.webdriver.chrome.service import Service
 
-# Добавление опции --language в командную строку
+
+# Добавление опций в командную строку
 def pytest_addoption(parser):
     parser.addoption('--language',
                      action='store',
                      default='en',
                      help="Choose language: es, fr, de, etc.")
 
+    parser.addoption('--browser',
+                     action='store',
+                     default='chrome',
+                     choices=['chrome', 'firefox', 'edge'],
+                     help="Choose browser: chrome, firefox, edge")
 
-# Фикстура для инициализации браузера с поддержкой разных языков
+
+# Фикстура для инициализации разных браузеров
 @pytest.fixture(scope="function")
 def browser(request):
+    browser_name = request.config.getoption("browser").lower()
     language = request.config.getoption("language")
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    browser = webdriver.Chrome(options=options)
 
-    yield browser
-    browser.quit()
+    if browser_name == "chrome":
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        driver = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        options = webdriver.FirefoxOptions()
+        options.set_preference("intl.accept_languages", language)
+        driver = webdriver.Firefox(options=options)
+
+    elif browser_name == "edge":
+        options = webdriver.EdgeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        driver = webdriver.Edge(options=options)
+
+    else:
+        raise pytest.UsageError(f"Unsupported browser: {browser_name}")
+
+    yield driver
+    driver.quit()
+
 
 # Фикстура для получения выбранного языка
 @pytest.fixture
